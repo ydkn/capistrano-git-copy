e547bf759fce880dc04c59347f7a11b952240898
Florian Schwab <me@ydkn.de>
2015-04-07 14:06:53 +0200

    strip whitespaces from branch

--- lib/capistrano/git_copy/utility.rb
+++ lib/capistrano/git_copy/utility.rb
@@ -152,7 +152,7 @@ module Capistrano
       def commit_hash
         return @_commit_hash if @_commit_hash
 
-        branch = fetch(:branch, 'master')
+        branch = fetch(:branch, 'master').strip
 
         if test! :git, 'rev-parse', "origin/#{branch}", '>/dev/null 2>/dev/null'
           @_commit_hash = capture(:git, 'rev-parse', "origin/#{branch}").strip
