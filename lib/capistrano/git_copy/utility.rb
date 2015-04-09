ae5fd9012fd41dab2e8d859d02321b6987984f7e
Erik-B. Ernst <erik.ernst@sic-software.com>
2015-04-09 07:54:59 +0200

    * strip branch name only if it is a string

--- lib/capistrano/git_copy/utility.rb
+++ lib/capistrano/git_copy/utility.rb
@@ -152,7 +152,8 @@ module Capistrano
       def commit_hash
         return @_commit_hash if @_commit_hash
 
-        branch = fetch(:branch, 'master').strip
+        branch = fetch(:branch, 'master')
+        branch.strip! if branch.is_a?(String)
 
         if test! :git, 'rev-parse', "origin/#{branch}", '>/dev/null 2>/dev/null'
           @_commit_hash = capture(:git, 'rev-parse', "origin/#{branch}").strip
