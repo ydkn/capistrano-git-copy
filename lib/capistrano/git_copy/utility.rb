fccfccbf3f7d56a12e93355ab1349ec3f956527b
Aswin S <techno.aswin@gmail.com>
2016-01-13 15:20:09 +0530

    Add support to deploy from a subdirectory of a git repo. Similar to repo_tree enhancement in https://github.com/capistrano/capistrano/blob/master/CHANGELOG.md#333

--- lib/capistrano/git_copy/utility.rb
+++ lib/capistrano/git_copy/utility.rb
@@ -10,6 +10,8 @@ module Capistrano
       def initialize(context)
         @context         = context
         @with_submodules = fetch(:with_submodules, true)
+        @repo_tree = fetch(:repo_tree, false)
+        @with_submodules = @repo_tree? false : true # If repo_tree is specified, set with_submodules to false
       end
 
       # Check if repo cache exists and is valid
@@ -71,10 +73,11 @@ module Capistrano
       #
       # @return void
       def prepare_release
+        target = @repo_tree? "HEAD:#{@repo_tree}" : "HEAD"
         if with_submodules
           execute(git_archive_all_bin, "--prefix=''", archive_path)
         else
-          git(:archive, '--format=tar', 'HEAD', '|', 'gzip', "> #{archive_path}")
+          git(:archive, '--format=tar', target, '|', 'gzip', "> #{archive_path}")
         end
       end
 
